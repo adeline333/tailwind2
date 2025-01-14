7aa20ef242d9987602e51249f8a3188e45af3ba7
Adeline Tuyizere <adelinetuyizere333@gmail.com>
2025-01-14 15:05:30 +0200

    getting started with my tailwind aftwr doing the setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/*.{html,js}",
+    // Add other paths for template files (e.g., ./src/**/*.{html,js})
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+
